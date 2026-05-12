7f5f170407bb875d41024f2e748ba2a487c680f6
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-05-12 23:24:06 +0300

    Task 1.1 done

---
+++ src/main/java/com/example/rxjava/paradigms/task1_1/Order.java
@@ -0,0 +1,4 @@
+package com.example.rxjava.paradigms.task1_1;
+
+public record Order(String id, Status status, double amount) {
+}
---
+++ src/main/java/com/example/rxjava/paradigms/task1_1/Status.java
@@ -0,0 +1,7 @@
+package com.example.rxjava.paradigms.task1_1;
+
+public enum Status {
+    DELIVERED,
+    PENDING,
+    CANCELLED
+}
